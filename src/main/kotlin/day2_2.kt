351c398b2ec1296e6a6554864362051a99d2869b
Fede Esteban <fede.est@gmail.com>
2025-12-02 17:45:14 -0300

    Improve readability on Day 2 solution

--- src/main/kotlin/day2_2.kt
+++ src/main/kotlin/day2_2.kt
@@ -1,19 +1,28 @@
 import java.io.File
+import kotlin.text.toLong
 
 fun main() {
     File("src/main/kotlin/resources/day2.txt").useLines { lines ->
         lines.forEach { line ->
             val result = line.split(",").fold(0L) { sum, range ->
                 val (first, last) = range.split("-")
-                sum + (first.toLong()..last.toLong()).fold(0L) { partialSum, n ->
-                    n.toString().let { repr ->
-                        partialSum + if ((1..repr.length / 2).any { patternSize ->
-                            repr.substring(0, patternSize).repeat(repr.length / patternSize) == repr
-                        }) n else 0
-                    }
-                }
+                sum + countMatchesInRange(first.toLong(), last.toLong())
             }
             print(result)
         }
+    }
+}
+
+private fun countMatchesInRange(first: Long, last: Long): Long {
+    return (first..last).fold(0L) { partialSum, n ->
+        n.toString().let { repr ->
+            partialSum + if (isRepeatingPattern(repr)) n else 0
+        }
+    }
+}
+
+private fun isRepeatingPattern(str: String): Boolean {
+    return (1..str.length / 2).any { patternSize ->
+        str.substring(0, patternSize).repeat(str.length / patternSize) == str
     }
 }
